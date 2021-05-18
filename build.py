4fed5fbdef9320463cd4960177c6ae2cfafc5cff
paddymills <paddymills@pm.me>
2021-05-18 13:06:01 -0400

    add build timestamp and output

--- build.py
+++ build.py
@@ -1,4 +1,7 @@
 
 import zipapp
+from datetime import datetime
 
 zipapp.create_archive("src", "menus\\application\\hss_dxf_export.zip")
+
+print("[{}] build complete.".format(datetime.now().isoformat()))
