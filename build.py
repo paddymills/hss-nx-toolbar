7a175f93d4a3070a89540acf01d89a6b09771cd0
paddymills <paddymills@pm.me>
2021-05-25 13:10:26 -0400

    add cargo build and copy .pyd

--- build.py
+++ build.py
@@ -1,6 +1,19 @@
 
+import os
+import shutil
 import zipapp
+
 from datetime import datetime
+
+FILEDIALOG_FROM = r"filedialog-rs\target\release\filedialog.dll"
+FILEDIALOG_TO = r"src\filedialog.pyd"
+
+# generate filedialog
+if not os.path.exists(FILEDIALOG_FROM):
+    os.system("cd filedialog-rs && cargo build --release && cd ..")
+
+# copy filedialog
+shutil.copy(FILEDIALOG_FROM, FILEDIALOG_TO)
 
 zipapp.create_archive("src", "menus\\application\\hss_dxf_export.zip")
 
