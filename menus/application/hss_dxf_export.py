022e7b839937d63c08fc870b226d869d385dfb34
paddymills <paddymills@pm.me>
2021-05-25 08:12:34 -0400

    add main

--- menus/application/hss_dxf_export.py
+++ menus/application/hss_dxf_export.py
@@ -2,13 +2,17 @@
 import os
 import runpy
 
-# This file is necessary because NX will not execute a .zip python file.
-# 
-# The workaround is for this file to execute the .zip python file.
-# This is not entirely necessary, but it keeps the parent folder clean.
+def main():
+    # This file is necessary because NX will not execute a .zip python file.
+    # 
+    # The workaround is for this file to execute the .zip python file.
+    # This is not entirely necessary, but it keeps the parent folder clean.
 
-exec_dir = os.path.dirname(__file__)
-zip_file = os.path.join(exec_dir, "hss_dxf_export.zip")
+    exec_dir = os.path.dirname(__file__)
+    zip_file = os.path.join(exec_dir, "hss_dxf_export.zip")
 
-# will run the __main__.py script within the zip file specified
-runpy.run_path(zip_file)
+    # will run the __main__.py script within the zip file specified
+    runpy.run_path(zip_file)
+
+if __name__ == "__main__":
+    main()
