f776bf14db6b6272930797a0c271737c631b2eef
paddymills <paddymills@pm.me>
2021-05-25 13:22:42 -0400

    copy pyd for test

--- filedialog-rs/test.py
+++ filedialog-rs/test.py
@@ -1,4 +1,10 @@
 
+import os
+import shutil
+
+this_dir = os.path.dirname(__file__)
+shutil.copy(os.path.join(this_dir, "target", "release", "filedialog.dll"), os.path.join(this_dir, "filedialog.pyd"))
 import filedialog
 
-print(filedialog.get_files(r"\\hssieng\hssedsserv\Jobs\dxf\test_files"))
+for f in filedialog.get_files(r"\\hssieng\hssedsserv\Jobs\dxf\test_files"):
+    print(f)
