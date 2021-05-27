8456a03f811d9041617041e34dfe545d20e301ac
paddymills <paddymills@pm.me>
2021-05-27 07:40:50 -0400

    split python and rust build

--- build.py
+++ build.py
@@ -1,22 +1,36 @@
 
 import os
+import sys
 import shutil
 import zipapp
 
 from datetime import datetime
 
-FILEDIALOG_FROM = r"filedialog-rs\target\release\filedialog.dll"
-FILEDIALOG_TO = r"menus\application\filedialog.pyd"
+def build_rs():
+    FILEDIALOG_FROM = r"filedialog-rs\target\release\filedialog.dll"
+    FILEDIALOG_TO = r"menus\application\filedialog.pyd"
 
-# generate filedialog
-if not os.path.exists(FILEDIALOG_FROM):
-    os.system("cd filedialog-rs && cargo build --release && cd ..")
+    # generate filedialog
+    if not os.path.exists(FILEDIALOG_FROM):
+        os.system("cd filedialog-rs && cargo build --release && cd ..")
 
-# copy filedialog
-shutil.copy(FILEDIALOG_FROM, FILEDIALOG_TO)
+    # copy filedialog
+    shutil.copy(FILEDIALOG_FROM, FILEDIALOG_TO)
 
-zipapp.create_archive("src", "menus\\application\\hss_dxf_export.zip")
-zipapp.create_archive("src", "C:\\Users\\PMiller1\\nx\\12\\application\\hss_dxf_export.zip")
-zipapp.create_archive("src", "C:\\Users\\PMiller1\\nx\\1953\\application\\hss_dxf_export.zip")
+    print("Rust build complete and exported")
 
-print("[{}] build complete.".format(datetime.now().isoformat()))
+
+def build_py():
+
+    zipapp.create_archive("src", "menus\\application\\hss_dxf_export.zip")
+    zipapp.create_archive("src", "C:\\Users\\PMiller1\\nx\\12\\application\\hss_dxf_export.zip")
+    zipapp.create_archive("src", "C:\\Users\\PMiller1\\nx\\1953\\application\\hss_dxf_export.zip")
+
+    print("[{}] build complete.".format(datetime.now().isoformat()))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "rs":
+        build_rs()
+
+    build_py()
