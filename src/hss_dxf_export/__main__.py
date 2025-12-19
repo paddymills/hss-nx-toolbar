8ff65f00a5d7c9b26c129f442c12735c2097add9
Patrick Miller <pimiller@high.net>
2025-12-19 13:06:35 -0500

    move to logger

--- src/hss_dxf_export/__main__.py
+++ src/hss_dxf_export/__main__.py
@@ -2,17 +2,17 @@
 from processor import get_processor_from_args
 
 import NXOpen
-import tracing
-
-logger = tracing.logger
+from tracing import info, error
+import os
+import dialog
 
 session = NXOpen.Session.GetSession()
 
 # log NX version
 nx_version = session.GetEnvironmentVariableValue("NX_FULL_VERSION")
-logger.info("NX Version: {}".format(nx_version))
+info("NX Version: {}".format(nx_version))
 
 try:
     get_processor_from_args().run()
 except Exception as e:
-    logger.error("Fatal error: {}".format(e), exc_info=True)
+    error("Fatal error: {}".format(e), exc_info=True)
